6a4ba026394658e20aace22215600b0cdc52c990
Yuli <yuliatiyuli39@gmail.com>
2019-12-01 23:16:19 +0800

    barcode-reader library implementation     installed barcode reader library using npm and is currently running     and reading data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+        <v-quagga :onDetected="logIt"  :readerSize="readerSize" :readerTypes="['ean_reader']"></v-quagga>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quagga from 'vue-quaggajs';
+import Vue from 'vue';
+
+Vue.use(Quagga);
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+
+    data () {
+    return {
+      readerSize: {
+        width: 640,
+        height: 480
+      },
+      detecteds: []
+    };
+  },
+  methods: {
+    logIt (data) {
+      console.log('detected', data);
+      console.log(data.codeResult.code);
+      console.log(Quagga.canvas); 
+    },
+
+    processed (data) {
+  let drawingCtx = Quagga.canvas.ctx.overlay,
+    drawingCanvas = Quagga.canvas.dom.overlay;
+ 
+  if (data) {
+    if (data.boxes) {
+      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+      data.boxes.filter(function (box) {
+        return box !== data.box;
+      }).forEach(function (box) {
+        Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+      });
+    }
+    if (data.box) {
+      Quagga.ImageDebug.drawPath(data.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+    }
+ 
+    if (data.codeResult && data.codeResult.code) {
+      Quagga.ImageDebug.drawPath(data.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+    }
   }
-}
+},
+
+  }
+ 
+};
 </script>
 
 <style>
